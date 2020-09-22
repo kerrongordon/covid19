4b890385c14a8a9e4330f241587ece1a9ad1d622
kerrongordon <kgpsounds.com@gmail.com>
2020-09-21 22:07:28 -0400

    added primary color 💥️

--- lib/themes/color-theme.dart
+++ lib/themes/color-theme.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ColorTheme {
-  static final Color cases = Color.fromRGBO(242, 185, 0, 1.0);
-  static final Color recovered = Color.fromRGBO(0, 204, 153, 1.0);
-  static final Color deaths = Color.fromRGBO(247, 99, 83, 1.0);
+  static final Color cases = const Color.fromRGBO(242, 185, 0, 1.0);
+  static final Color recovered = const Color.fromRGBO(0, 204, 153, 1.0);
+  static final Color deaths = const Color.fromRGBO(247, 99, 83, 1.0);
+  static final Color primary = const Color.fromRGBO(0, 133, 255, 1.0);
 }
