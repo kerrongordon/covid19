f86c0223d4da04532acc0970f205d4df1acd42c6
kerrongordon <kgpsounds.com@gmail.com>
2020-09-21 22:57:25 -0400

    added option to remove hero tag

--- lib/components/kgp-flag.dart
+++ lib/components/kgp-flag.dart
@@ -16,27 +16,30 @@ class KgpFlag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: tag,
-      child: CachedNetworkImage(
-        imageUrl: imageUrl,
-        imageBuilder: (context, imageProvider) => Container(
-          width: imageWidth,
-          height: imageHeight,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.cover,
-            ),
+    return tag == null
+        ? imageBuilderUi()
+        : Hero(tag: tag, child: imageBuilderUi());
+  }
+
+  CachedNetworkImage imageBuilderUi() {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      imageBuilder: (context, imageProvider) => Container(
+        width: imageWidth,
+        height: imageHeight,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: imageProvider,
+            fit: BoxFit.cover,
           ),
         ),
-        errorWidget: (context, url, error) => Icon(Icons.error),
-        placeholder: (context, url) => Container(
-          child: CircularProgressIndicator(strokeWidth: 2.0),
-          width: imageWidth,
-          height: imageHeight,
-        ),
+      ),
+      errorWidget: (context, url, error) => const Icon(Icons.error),
+      placeholder: (context, url) => Container(
+        child: const CircularProgressIndicator(strokeWidth: 2.0),
+        width: imageWidth,
+        height: imageHeight,
       ),
     );
   }
