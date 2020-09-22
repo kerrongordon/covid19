49dfb64a523b6499e0a4126bf8a10f08fe4db9fa
kerrongordon <kgpsounds.com@gmail.com>
2020-09-21 21:04:12 -0400

    new Tab screen using Hooks 🤩️🤯️

---
+++ lib/screens/tab/tab-bottom-bar.dart
@@ -0,0 +1,63 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class TabBottomBar extends StatelessWidget {
+  const TabBottomBar({
+    Key key,
+    @required ValueNotifier<int> pageIndex,
+    @required this.onItemSelected,
+  })  : _pageIndex = pageIndex,
+        super(key: key);
+
+  final ValueNotifier<int> _pageIndex;
+  final void Function(int) onItemSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
+    return BottomNavyBar(
+      backgroundColor: _theme.cardTheme.color,
+      selectedIndex: _pageIndex.value,
+      showElevation: true,
+      onItemSelected: onItemSelected,
+      items: <BottomNavyBarItem>[
+        BottomNavyBarItem(
+          title: const Text('Global'),
+          textAlign: TextAlign.center,
+          icon: const Icon(Ionicons.ios_analytics),
+          activeColor: _theme.accentColor,
+          inactiveColor: _theme.textTheme.bodyText1.color,
+        ),
+        BottomNavyBarItem(
+          title: const Text('Countries'),
+          textAlign: TextAlign.center,
+          icon: const Icon(Ionicons.ios_trending_up),
+          activeColor: _theme.accentColor,
+          inactiveColor: _theme.textTheme.bodyText1.color,
+        ),
+        BottomNavyBarItem(
+          title: const Text('Home'),
+          textAlign: TextAlign.center,
+          icon: const Icon(Ionicons.ios_pin),
+          activeColor: _theme.accentColor,
+          inactiveColor: _theme.textTheme.bodyText1.color,
+        ),
+        BottomNavyBarItem(
+          title: const Text('Info'),
+          textAlign: TextAlign.center,
+          icon: const Icon(Ionicons.ios_apps),
+          activeColor: _theme.accentColor,
+          inactiveColor: _theme.textTheme.bodyText1.color,
+        ),
+        BottomNavyBarItem(
+          title: const Text('Settings'),
+          textAlign: TextAlign.center,
+          icon: const Icon(Ionicons.ios_cog),
+          activeColor: _theme.accentColor,
+          inactiveColor: _theme.textTheme.bodyText1.color,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/tab/tab-screen.dart
@@ -0,0 +1,51 @@
+import 'package:covid19/hooks/page.controller.hook.dart';
+import 'package:covid19/screens/tab/tab-bottom-bar.dart';
+import 'package:covid19/screens/countries/countries-screen.dart';
+import 'package:covid19/screens/global/global-screen.dart';
+import 'package:covid19/screens/information/infor-screen.dart';
+import 'package:covid19/screens/main/main-screen.dart';
+import 'package:covid19/screens/settings/settings-screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class TabScreen extends HookWidget {
+  TabScreen({Key key}) : super(key: key);
+
+  final MainScreen _mainScreen = MainScreen();
+  final GlobalScreen _homeScreen = GlobalScreen();
+  final InforScreen _inforScreen = InforScreen();
+  final SettingsScreen _settingsScreen = SettingsScreen();
+  final CountriesScreen _countriesScreen = CountriesScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    final _pageIndex = useState(2);
+    final _pageController = usePageController(initialPage: _pageIndex.value);
+
+    void onPageChange(int index) => _pageIndex.value = index;
+
+    void onTapIcon(int index) => _pageController.animateToPage(
+          index,
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeInOut,
+        );
+
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: onPageChange,
+        children: [
+          _homeScreen,
+          _countriesScreen,
+          _mainScreen,
+          _inforScreen,
+          _settingsScreen,
+        ],
+      ),
+      bottomNavigationBar: TabBottomBar(
+        pageIndex: _pageIndex,
+        onItemSelected: onTapIcon,
+      ),
+    );
+  }
+}
