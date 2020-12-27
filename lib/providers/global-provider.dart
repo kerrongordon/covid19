bc5e20d27b8ef08aafccf8f832b8cdc4f7858998
kerrongordon <kgpsounds.com@gmail.com>
2020-12-27 00:55:14 -0400

    Global Service update

--- lib/providers/global-provider.dart
+++ lib/providers/global-provider.dart
@@ -1,8 +1,6 @@
-import 'package:covid19/models/global-model.dart';
 import 'package:covid19/services/global-service.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final globalProvider = FutureProvider.autoDispose((_) async {
-  final res = await GlobalService().getGlobalApi();
-  return Global.fromJson(res.data);
-});
+final globalProvider = FutureProvider.autoDispose(
+  (_) async => await GlobalService().getGlobalApi(),
+);
