8a08dc416582234313d9d054ab1a6889ad90b0bd
kerrongordon <kgpsounds.com@gmail.com>
2020-12-29 15:48:41 -0400

    update teavel

--- lib/screens/country/travel-alert-screen.dart
+++ lib/screens/country/travel-alert-screen.dart
@@ -1,37 +1,23 @@
 import 'package:covid19/components/country-card/country-card-travel-alert.dart';
 import 'package:covid19/models/country-model.dart';
-import 'package:covid19/models/travel-alert-model.dart';
-import 'package:covid19/providers/travel-alert-provider.dart';
+import 'package:covid19/screens/country/providers/travel-alert.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TravelAlertScreen extends ConsumerWidget {
+class TravelAlertScreen extends HookWidget {
   final Country data;
-  TravelAlertScreen({
-    @required this.data,
-  });
+  const TravelAlertScreen({this.data});
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  Widget build(BuildContext context) {
     final couCode = data.countryInfo.iso2;
-    final travelAlert = watch(travelAlertProvider);
+    final travel = useProvider(travelAlert(couCode));
 
-    return FutureBuilder(
-      future: travelAlert.gettravelAlertApi(countrycode: couCode),
-      builder: (context, AsyncSnapshot<TravelAlert> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Container();
-        } else if (snapshot.hasError) {
-          return Container();
-        } else if (snapshot.hasData) {
-          final datal = snapshot.data.data;
-          return datal.values.length == 1
-              ? CountryCardTravelAlert(data: datal.values.first)
-              : Container();
-        } else {
-          return Container();
-        }
-      },
+    return travel.when(
+      data: (data) => CountryCardTravelAlert(data: data.data.values.first),
+      loading: () => Container(),
+      error: (error, _) => Container(),
     );
   }
 }
