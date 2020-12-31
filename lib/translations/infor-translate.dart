0b8a16f90978db33592be31fd993f08a8ea757cb
kerrongordon <kgpsounds.com@gmail.com>
2020-12-30 23:30:56 -0400

    infor translate

---
+++ lib/translations/infor-translate.dart
@@ -0,0 +1,16 @@
+import 'package:easy_localization/easy_localization.dart';
+
+final String pagetitle = tr('inforscreen.pagetitle');
+
+final String inforonetitle = tr('inforscreen.inforone.title');
+final String inforonedescription = tr('inforscreen.inforone.description');
+final String infortwotitle = tr('inforscreen.infortwo.title');
+final String infortwodescription = tr('inforscreen.infortwo.description');
+final String inforthreetitle = tr('inforscreen.inforthree.title');
+final String inforthreedescription = tr('inforscreen.inforthree.description');
+final String inforfourtitle = tr('inforscreen.inforfour.title');
+final String inforfourdescription = tr('inforscreen.inforfour.description');
+final String inforfivetitle = tr('inforscreen.inforfive.title');
+final String inforfivedescription = tr('inforscreen.inforfive.description');
+final String inforsixtitle = tr('inforscreen.inforsix.title');
+final String inforsixdescription = tr('inforscreen.inforsix.description');
