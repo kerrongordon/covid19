76a8d49acbb0ee9314f23061a4e333c303316742
kerron <kgpsounds.com@gmail.com>
2021-01-02 18:52:58 -0400

    update ad component

--- lib/components/ads-component.dart
+++ lib/components/ads-component.dart
@@ -1,94 +1,70 @@
-import 'dart:async';
+// import 'dart:async';
 
 import 'package:covid19/components/card-component.dart';
 import 'package:covid19/themes/color-theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_native_admob/flutter_native_admob.dart';
 import 'package:flutter_native_admob/native_admob_controller.dart';
 import 'package:flutter_native_admob/native_admob_options.dart';
 
-class AdsComponent extends StatefulWidget {
+class AdsComponent extends HookWidget {
   final NativeAdmobType type;
-  final Color color;
-  final Color textColor;
-  const AdsComponent({this.type, this.color, this.textColor});
+  const AdsComponent({this.type});
 
-  @override
-  _AdsComponentState createState() => _AdsComponentState();
-}
-
-class _AdsComponentState extends State<AdsComponent> {
   static const _adUnitID = 'ca-app-pub-5675929973866541/7003986965';
-  final _nativeAdController = NativeAdmobController();
-  double _height = 0;
-
-  StreamSubscription _subscription;
-
-  @override
-  void initState() {
-    _nativeAdController.setTestDeviceIds(['D1A9FB043D5B3B683FFB73575B9C8AD8']);
-    _subscription = _nativeAdController.stateChanged.listen(_onStateChanged);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _subscription.cancel();
-    _nativeAdController.dispose();
-    super.dispose();
-  }
-
-  void _onStateChanged(AdLoadState state) {
-    switch (state) {
-      case AdLoadState.loading:
-        setState(() {
-          _height = 0;
-        });
-        break;
-
-      case AdLoadState.loadCompleted:
-        setState(() {
-          _height = widget.type == NativeAdmobType.full ? 350 : 150;
-        });
-        break;
-
-      default:
-        break;
-    }
-  }
+  static const testDevice = ['D1A9FB043D5B3B683FFB73575B9C8AD8'];
 
   @override
   Widget build(BuildContext context) {
+    final _height = useState(0.0);
+    final _nativeAdmobController = useState(NativeAdmobController());
     final theme = Theme.of(context);
     final textColor = theme.textTheme.bodyText1.color;
+
+    void _onStateChanged(AdLoadState state) {
+      switch (state) {
+        case AdLoadState.loading:
+          _height.value = 0;
+          break;
+        case AdLoadState.loadCompleted:
+          _height.value = type == NativeAdmobType.full ? 350 : 150;
+          break;
+        case AdLoadState.loadError:
+          _height.value = 0;
+          break;
+      }
+    }
+
+    _nativeAdmobController.value.setTestDeviceIds(testDevice);
+
+    useEffect(() {
+      final _subscription =
+          _nativeAdmobController.value.stateChanged.listen(_onStateChanged);
+      return () => _subscription.cancel();
+    }, [_nativeAdmobController.value.stateChanged]);
+
     return Container(
-      height: _height,
+      height: _height.value,
       child: CardComponent(
-        color: widget.color,
         padding: const EdgeInsets.all(20),
         child: NativeAdmob(
           adUnitID: _adUnitID,
-          controller: _nativeAdController,
+          controller: _nativeAdmobController.value,
           loading: Container(),
           error: Container(),
-          type: widget.type,
+          type: type,
           options: NativeAdmobOptions(
             callToActionStyle: NativeTextStyle(
               backgroundColor: theme.accentColor,
               fontSize: 12,
             ),
-            adLabelTextStyle:
-                NativeTextStyle(color: widget.textColor ?? textColor),
-            bodyTextStyle:
-                NativeTextStyle(color: widget.textColor ?? textColor),
-            headlineTextStyle:
-                NativeTextStyle(color: widget.textColor ?? textColor),
-            advertiserTextStyle:
-                NativeTextStyle(color: widget.textColor ?? textColor),
-            priceTextStyle:
-                NativeTextStyle(color: widget.textColor ?? textColor),
-            storeTextStyle:
-                NativeTextStyle(color: widget.textColor ?? textColor),
+            adLabelTextStyle: NativeTextStyle(color: textColor),
+            bodyTextStyle: NativeTextStyle(color: textColor),
+            headlineTextStyle: NativeTextStyle(color: textColor),
+            advertiserTextStyle: NativeTextStyle(color: textColor),
+            priceTextStyle: NativeTextStyle(color: textColor),
+            storeTextStyle: NativeTextStyle(color: textColor),
             ratingColor: ColorTheme.cases,
           ),
         ),
