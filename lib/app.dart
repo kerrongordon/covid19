c286292b8b6355d25e2d5a3a51658b5fa59cf9ce
kerron <kgpsounds.com@gmail.com>
2021-01-10 08:47:43 -0400

    boarding screen

--- lib/app.dart
+++ lib/app.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
