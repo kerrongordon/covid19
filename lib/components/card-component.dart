a75822549961a08567fe6df54fe43d842f6a6c45
kerrongordon <kgpsounds.com@gmail.com>
2021-02-08 11:51:46 -0400

    remove slow animation

--- lib/components/card-component.dart
+++ lib/components/card-component.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animator/flutter_animator.dart';
 
 class CardComponent extends StatelessWidget {
   final Color color;
@@ -19,25 +18,23 @@ class CardComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return FadeInUp(
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        color: color ?? theme.cardTheme.color,
-        elevation: 20,
-        shadowColor: color != null
-            ? color.withOpacity(0.9)
-            : Colors.black.withOpacity(0.4),
-        margin: margin ?? const EdgeInsets.only(bottom: 20, top: 10),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: InkWell(
-          splashColor: theme.primaryColor,
-          onTap: onTap,
-          child: Padding(
-            padding: padding ?? const EdgeInsets.all(0),
-            child: child,
-          ),
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      color: color ?? theme.cardTheme.color,
+      elevation: 20,
+      shadowColor: color != null
+          ? color.withOpacity(0.9)
+          : Colors.black.withOpacity(0.4),
+      margin: margin ?? const EdgeInsets.only(bottom: 20, top: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        splashColor: theme.primaryColor,
+        onTap: onTap,
+        child: Padding(
+          padding: padding ?? const EdgeInsets.all(0),
+          child: child,
         ),
       ),
     );
